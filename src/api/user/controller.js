198f33653068b8afdd4bd76cce72d5e846363ce1
subin0201 <tyjk431@gmail.com>
2023-01-16 15:01:57 +0900

    설정 파일 분리

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = 'my-secret-key';
+// const SECRET_KEY = 'my-secret-key';
 
 // 해당 id의 회원정보들
 exports.info = (ctx, next) => {
@@ -33,7 +33,7 @@ exports.login = async (ctx, next) => {
 
 let generteToken = (payload) => {
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, SECRET_KEY, (error, token) => {
+        jwt.sign(payload, process.env.APP_KEY, (error, token) => {
             // if(error) { reject(error); }
             // resolve(token);
             (error) ? reject(error) : resolve(token);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'my-secret-key';
+// const SECRET_KEY = 'my-secret-key';
 
 exports.verify = async (ctx, next) => {
     var token = ctx.request.headers['token'];   // headers에 토큰을 보내는 방법. get post delete 아무거나 상관없음
-    jwt.verify(token, SECRET_KEY, (error, decoded) => {  // 토큰과 키 기반으로 에러 검출
+    jwt.verify(token, process.env.APP_KEY, (error, decoded) => {  // 토큰과 키 기반으로 에러 검출
         if(error) {
             ctx.body = '로그인을 해야합니다.';
             return;
